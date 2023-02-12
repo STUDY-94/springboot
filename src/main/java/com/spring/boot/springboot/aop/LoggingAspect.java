01c2db321f94fd491cd29787946bcad776528198
ssolly-choi <ssolly.choi@gmail.com>
2023-02-12 16:55:25 +0900

    aop : 기본 설정 및 적용

---
+++ src/main/java/com/spring/boot/springboot/aop/LoggingAspect.java
@@ -0,0 +1,41 @@
+package com.spring.boot.springboot.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private long startTime;
+
+    @Before("execution(public String||boolean com.spring..*.*(..))")
+    public void onBeforeHandler(JoinPoint joinPoint) {
+        startTime = System.nanoTime();
+        System.out.println("start : " + startTime);
+    }
+
+    @After("execution(* com.spring..*(..))")
+    public void onAfterHandler(JoinPoint joinPoint) {
+        long endTime = System.nanoTime();
+        System.out.println("elapsed time : " + (endTime - startTime));
+    }
+
+    @Around("execution(* check*()) && within(com.spring..*)")
+    public String around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        String returnValue = null;
+        try {
+            returnValue = (String) proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        returnValue = "**" + returnValue + "**";
+        return  returnValue;
+    }
+}
---
+++ src/main/java/com/spring/boot/springboot/controller/HotelController.java
@@ -0,0 +1,34 @@
+package com.spring.boot.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelController {
+
+    @GetMapping("/in")
+    @ResponseBody
+    public String checkIn() {
+        return "check-in";
+    }
+
+    @GetMapping("/out")
+    @ResponseBody
+    public String checkOut() {
+        return "check-out";
+    }
+
+    @GetMapping("/inout")
+    @ResponseBody
+    public int inOut() {
+        return 1;
+    }
+
+    @GetMapping("/outin")
+    @ResponseBody
+    public boolean outIn() {
+        return true;
+    }
+
+}
